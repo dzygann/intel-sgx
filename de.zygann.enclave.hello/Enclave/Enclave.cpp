b55214aceddc66e110bec6864c372882d63d4c8f
Denis Zygann <denis.zygann@outlook.com>
2021-05-03 08:25:17 +0200

    Initial implementation of Hello World Enclave

---
+++ de.zygann.enclave.hello/Enclave/Enclave.cpp
@@ -0,0 +1,33 @@
+/*
+ * Enclave.cpp
+ *
+ *  Created on: May 2, 2021
+ *      Author: Denis Zygann
+ */
+
+#include "Enclave.h"
+#include "Enclave_t.h" /* print_string */
+#include <stdarg.h>
+#include <stdio.h> /* vsnprintf */
+#include <string.h>
+
+/*
+ * printf:
+ *   Invokes OCALL to display the enclave buffer to the terminal.
+ */
+int printf(const char* fmt, ...)
+{
+    char buf[BUFSIZ] = { '\0' };
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(buf, BUFSIZ, fmt, ap);
+    va_end(ap);
+    ocall_print_string(buf);
+    return (int)strnlen(buf, BUFSIZ - 1) + 1;
+}
+
+void printf_helloworld(){
+	printf("Hello World\n");
+}
+
+
---
+++ de.zygann.enclave.hello/Enclave/Enclave.h
@@ -0,0 +1,25 @@
+/*
+ * Enclave.h
+ *
+ *  Created on: May 2, 2021
+ *      Author: Denis Zygann
+ */
+
+#ifndef _ENCLAVE_H_
+#define _ENCLAVE_H_
+
+#include <assert.h>
+#include <stdlib.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+int printf(const char* fmt, ...);
+
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* !_ENCLAVE_H_ */
